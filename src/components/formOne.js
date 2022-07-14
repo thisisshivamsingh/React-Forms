6f68a87def92d0c505561cbe2fc46ab8767bebf1
Shivam <thisisshivam06@gmail.com>
2022-07-14 13:03:25 +0530

    Formik-Validation

--- src/components/formOne.js
+++ src/components/formOne.js
@@ -11,6 +11,25 @@ const FormOne = () => {
         state: "",
         zip: "",
       }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.firstname) {
+          errors.firstname = "Sorry, this is required";
+        }
+        if (!values.lastname) {
+          errors.lastname = "Sorry, this is required";
+        }
+        if (!values.email) {
+          errors.email = "Sorry, this is required";
+        } else if (
+          !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            values.email
+          )
+        ) {
+          errors.email = "invalid email";
+        }
+        return errors;
+      }}
       onSubmit={() => {
         console.log("Form submitted");
       }}
@@ -40,6 +59,7 @@ const FormOne = () => {
                     value={values.firstname}
                     onChange={handleChange}
                   />
+                  {errors.firstname ? <span>{errors.firstname}</span> : null}
                 </div>
                 <div className="col-md-6 mb-3">
                   <label htmlFor="lastname">Last name</label>
@@ -48,7 +68,10 @@ const FormOne = () => {
                     className="form-control"
                     id="lastname"
                     name="lastname"
+                    value={values.lastname}
+                    onChange={handleChange}
                   />
+                  {errors.lastname ? <span>{errors.lastname}</span> : null}
                 </div>
               </div>
 
@@ -60,7 +83,10 @@ const FormOne = () => {
                   id="email"
                   name="email"
                   placeholder="you@example.com"
+                  value={values.email}
+                  onChange={handleChange}
                 />
+                {errors.email ? <span>{errors.email}</span> : null}
               </div>
 
               <div className="row">
